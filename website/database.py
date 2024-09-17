124031c30e83ab6d4fbf26f9a0a51c3a278abf22
CHEETAH02 <chetansathish02@gmail.com>
2024-09-17 18:45:58 +0530

    Database Changes

--- website/database.py
+++ website/database.py
@@ -1,10 +1,8 @@
 from pymongo.mongo_client import MongoClient
 import os
 
-DB_USERNAME = 'shortenURL_user'
-# DB_USERNAME = os.environ.get('DB_USERNAME')
-DB_PASSWORD = 'Kes87UbIKCiGSxtS'
-# DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 uri = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@cluster0.3fajycc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
